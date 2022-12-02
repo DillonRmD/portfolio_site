fd30f6a2767c19a4771c95efe5b300f038e79ea6
DillonRmD <dillonwilliams2001@yahoo.com>
2022-12-02 16:04:38 -0600

    Added projects to have multiple images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,213 +5,245 @@ import Grid from "@mui/material/Grid";
 import { languages, APIs, tools, databases } from "./Skills";
 
 const About = () => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        backgroundImage: "url(https://i.gifer.com/Ozp.gif)",
-        backgroundSize: "100% 100%",
-      }}
-    >
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        padding={5}
-      >
-        <div>
-          <p
-            style={{
-              fontWeight: "bold",
-              color: "white",
-              fontFamily: "Nunito",
-            }}
-          >
-            Try Clicking My Headshot!
-          </p>
-
-          <a href="https://www.linkedin.com/in/dillon-s-williams/" target="_blank" rel="noreferrer">
-            <ReactRoundedImage
-              image={MyProfilePic}
-              roundedColor="white"
-              roundedSize="5"
-              borderRadius="500"
-              hoverColor="#DD1144"
-            />
-          </a>
-        </div>
-
+    return (
         <div
-          style={{
-            fontFamily: "Nunito",
-            color: "white",
-            fontSize: "24px",
-            textAlign: "center",
-          }}
-        >
-          <p style={{ fontWeight: "bold" }}>Hey, my name is Dillon!</p>
-          <p style={{ fontSize: "20px" }}>
-            Fullstack Software Engineer 
-          </p>
-          <p style={{ fontSize: "20px" }}>
-            Life-long dedication to learning!
-          </p>
-          <p style={{ fontSize: "20px" }}>
-            Specialization in web development, low-level systems, and graphics programming!
-          </p>
-          <p style={{ marginTop: "5%", fontWeight: "bold" }}>
-            Education:
-          </p>
-          <p style={{ fontSize: "20px", color:"orange"}}>BS in Computer Science from Oklahoma State University!</p>
-          <p
             style={{
-              color: "white",
-              fontWeight: "bold",
-              textAlign: "center",
-              fontSize: "24px",
-              fontFamily: "Nunito",
-              marginTop: "5%",
+                display: "flex",
+                backgroundImage: "url(https://i.gifer.com/Ozp.gif)",
+                backgroundSize: "100% 100%",
             }}
-          >
-            Skill Set
-          </p>
+        >
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                padding={5}
+            >
+                <div>
+                    <p
+                        style={{
+                            fontWeight: "bold",
+                            color: "white",
+                            fontFamily: "Nunito",
+                        }}
+                    >
+                        Try Clicking My Headshot!
+                    </p>
+
+                    <a
+                        href="https://www.linkedin.com/in/dillon-s-williams/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <ReactRoundedImage
+                            image={MyProfilePic}
+                            roundedColor="white"
+                            roundedSize="5"
+                            borderRadius="500"
+                            hoverColor="#DD1144"
+                        />
+                    </a>
+                </div>
+
+                <div
+                    style={{
+                        fontFamily: "Nunito",
+                        color: "white",
+                        fontSize: "24px",
+                        textAlign: "center",
+                    }}
+                >
+                    <p style={{ fontWeight: "bold" }}>
+                        Hey, my name is Dillon!
+                    </p>
+                    <p style={{ fontSize: "20px" }}>
+                        Fullstack Software Engineer
+                    </p>
+                    <p style={{ fontSize: "20px" }}>
+                        Life-long dedication to learning!
+                    </p>
+                    <p style={{ fontSize: "20px" }}>
+                        Specialization in web development, low-level systems,
+                        and graphics programming!
+                    </p>
+                    <p style={{ marginTop: "5%", fontWeight: "bold" }}>
+                        Education:
+                    </p>
+                    <p style={{ fontSize: "20px", color: "orange" }}>
+                        BS in Computer Science from Oklahoma State University!
+                    </p>
+                    <p
+                        style={{
+                            color: "white",
+                            fontWeight: "bold",
+                            textAlign: "center",
+                            fontSize: "24px",
+                            fontFamily: "Nunito",
+                            marginTop: "5%",
+                        }}
+                    >
+                        Skill Set
+                    </p>
+                </div>
+
+                <Grid
+                    container
+                    spacing={0}
+                    padding={2}
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <p
+                        style={{
+                            color: "white",
+                            textAlign: "center",
+                            fontSize: "20px",
+                            fontFamily: "Nunito",
+                        }}
+                    >
+                        Languages
+                    </p>
+
+                    {languages.map((lang, langIndex) => (
+                        <div
+                            key={langIndex}
+                            style={{
+                                color: "white",
+                                textAlign: "center",
+                                fontsize: "8px",
+                                fontFamily: "Nunito",
+                            }}
+                        >
+                            <img
+                                alt={lang.name}
+                                src={lang.image}
+                                width="40"
+                                height="40"
+                            />
+                            <p>{lang.name}</p>
+                        </div>
+                    ))}
+                </Grid>
+
+                <Grid
+                    container
+                    spacing={0}
+                    padding={2}
+                    direction="row"
+                    justifyContent="space-around"
+                    alignItems="center"
+                >
+                    <p
+                        style={{
+                            color: "white",
+                            textAlign: "center",
+                            fontSize: "20px",
+                            fontFamily: "Nunito",
+                        }}
+                    >
+                        Frameworks
+                    </p>
+                    {APIs.map((api, apiIndex) => (
+                        <div
+                            key={apiIndex}
+                            style={{
+                                color: "white",
+                                textAlign: "center",
+                                fontsize: "8px",
+                                fontFamily: "Nunito",
+                            }}
+                        >
+                            <img
+                                alt={api.name}
+                                src={api.image}
+                                width="40"
+                                height="40"
+                            />
+                            <p>{api.name}</p>
+                        </div>
+                    ))}
+                </Grid>
+                <p
+                    style={{
+                        color: "white",
+                        textAlign: "center",
+                        fontSize: "20px",
+                        fontFamily: "Nunito",
+                    }}
+                >
+                    Tools
+                </p>
+                <Grid
+                    container
+                    spacing={0}
+                    padding={2}
+                    direction="row"
+                    justifyContent="space-around"
+                    alignItems="center"
+                >
+                    {tools.map((tool, toolIndex) => (
+                        <div
+                            key={toolIndex}
+                            style={{
+                                color: "white",
+                                textAlign: "center",
+                                fontsize: "8px",
+                                fontFamily: "Nunito",
+                            }}
+                        >
+                            <img
+                                alt={tool.name}
+                                src={tool.image}
+                                width="40"
+                                height="40"
+                            />
+                            <p>{tool.name}</p>
+                        </div>
+                    ))}
+                </Grid>
+                <p
+                    style={{
+                        color: "white",
+                        textAlign: "center",
+                        fontSize: "20px",
+                        fontFamily: "Nunito",
+                    }}
+                >
+                    Databases
+                </p>
+                <Grid
+                    container
+                    spacing={0}
+                    padding={2}
+                    direction="row"
+                    justifyContent="space-around"
+                    alignItems="center"
+                >
+                    {databases.map((db, dbIndex) => (
+                        <div
+                            key={dbIndex}
+                            style={{
+                                color: "white",
+                                textAlign: "center",
+                                fontsize: "8px",
+                                fontFamily: "Nunito",
+                            }}
+                        >
+                            <img
+                                alt={db.name}
+                                src={db.image}
+                                width="40"
+                                height="40"
+                            />
+                            <p>{db.name}</p>
+                        </div>
+                    ))}
+                </Grid>
+            </Grid>
         </div>
-        <p
-          style={{
-            color: "white",
-            textAlign: "center",
-            fontSize: "20px",
-            fontFamily: "Nunito",
-          }}
-        >
-          Languages
-        </p>
-        <Grid
-          container
-          spacing={0}
-          padding={2}
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          {languages.map((lang, langIndex) => (
-            <div
-              key={langIndex}
-              style={{
-                color: "white",
-                textAlign: "center",
-                fontsize: "8px",
-                fontFamily: "Nunito",
-              }}
-            >
-              <img alt={lang.name} src={lang.image} width="40" height="40" />
-              <p>{lang.name}</p>
-            </div>
-          ))}
-        </Grid>
-        <p
-          style={{
-            color: "white",
-            textAlign: "center",
-            fontSize: "20px",
-            fontFamily: "Nunito",
-          }}
-        >
-          Frameworks
-        </p>
-        <Grid
-          container
-          spacing={0}
-          padding={2}
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          {APIs.map((api, apiIndex) => (
-            <div
-              key={apiIndex}
-              style={{
-                color: "white",
-                textAlign: "center",
-                fontsize: "8px",
-                fontFamily: "Nunito",
-              }}
-            >
-              <img alt={api.name} src={api.image} width="40" height="40" />
-              <p>{api.name}</p>
-            </div>
-          ))}
-        </Grid>
-        <p
-          style={{
-            color: "white",
-            textAlign: "center",
-            fontSize: "20px",
-            fontFamily: "Nunito",
-          }}
-        >
-          Tools
-        </p>
-        <Grid
-          container
-          spacing={0}
-          padding={2}
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          {tools.map((tool, toolIndex) => (
-            <div
-              key={toolIndex}
-              style={{
-                color: "white",
-                textAlign: "center",
-                fontsize: "8px",
-                fontFamily: "Nunito",
-              }}
-            >
-              <img alt={tool.name} src={tool.image} width="40" height="40" />
-              <p>{tool.name}</p>
-            </div>
-          ))}
-        </Grid>
-        <p
-          style={{
-            color: "white",
-            textAlign: "center",
-            fontSize: "20px",
-            fontFamily: "Nunito",
-          }}
-        >
-          Databases
-        </p>
-        <Grid
-          container
-          spacing={0}
-          padding={2}
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          {databases.map((db, dbIndex) => (
-            <div
-              key={dbIndex}
-              style={{
-                color: "white",
-                textAlign: "center",
-                fontsize: "8px",
-                fontFamily: "Nunito",
-              }}
-            >
-              <img alt={db.name} src={db.image} width="40" height="40" />
-              <p>{db.name}</p>
-            </div>
-          ))}
-        </Grid>
-      </Grid>
-    </div>
-  );
+    );
 };
 
 export default About;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,88 +2,115 @@ import React from "react";
 import "./Project.css";
 
 function Project() {
-    
-  const projects = [
-    {
-      name: "Dillon's Tool Box (dtb)",
-      description:
-        "A collection of single C/C++ header files to simplify different functionality in the C/C++ language",
-      image: "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/cplusplus/cplusplus-original.svg",
-      link: "https://github.com/DillonRmD/dtb",
-    },
-    {
-      name: "Conway's Game Of Life",
-      description: "A implementation of Conway's Game Of Life Featuring React and Typescript",
-      image: "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original.svg",
-      link: "https://dw-game-of-life.netlify.app/",
-    },
-    {
-      name: "Elusive Rabbit",
-      description:
-        "A simulation and algorithm application to find an elusive rabbit that hops from hole to hole.",
-      image: "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/python/python-original.svg",
-      link: "https://github.com/DillonRmD/ElusiveRabbit",
-    },
-    {
-        name: "Movie Catalog",
-        description: "A WPF and C# desktop application that utilizes a T-SQL database to catalog and rate watched movies",
-        image: "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/csharp/csharp-original.svg",
-        link: "https://github.com/DillonRmD/Visual-Algorithms"
-    },
-    {
-      name: "Software Renderer",
-      description: "Basic 2D software renderer written for the win32 platform layer in C/C++",
-      image: "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/cplusplus/cplusplus-original.svg",
-      link: "https://github.com/DillonRmD/SoftwareRenderer",
-    },
-    {
-        name: "Literally Just A Space Game With Cards",
-        description: "A Collaborative Unity 2D, game project that features dynamic card playing in a vast universe just waiting to be conquered!",
-        image: "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/unity/unity-original.svg",
-        link: "https://github.com/aking618/LJSGC",
-    },
+    const projects = [
+        {
+            name: "Dillon's Tool Box (dtb)",
+            description:
+                "A collection of single C/C++ header files to simplify different functionality in the C/C++ language",
+            image: [
+                "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/cplusplus/cplusplus-original.svg",
+            ],
+            link: "https://github.com/DillonRmD/dtb",
+        },
+        {
+            name: "Conway's Game Of Life",
+            description:
+                "A implementation of Conway's Game Of Life Featuring React and Typescript",
+            image: [
+                "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original.svg",
+                "https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg"
+            ],
+            link: "https://dw-game-of-life.netlify.app/",
+        },
+        {
+            name: "Elusive Rabbit",
+            description:
+                "A simulation and algorithm application to find an elusive rabbit that hops from hole to hole.",
+            image: ["https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/python/python-original.svg"],
+            link: "https://github.com/DillonRmD/ElusiveRabbit",
+        },
+        {
+            name: "Movie Catalog",
+            description:
+                "A WPF and C# desktop application that utilizes a T-SQL database to catalog and rate watched movies",
+            image: [
+              "https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/csharp/csharp-original.svg",
+              "https://spin.atomicobject.com/wp-content/uploads/Microsoft_WPF.png"
+            ],
+            link: "https://github.com/DillonRmD/Visual-Algorithms",
+        },
+        {
+            name: "Software Renderer",
+            description:
+                "Basic 2D software renderer written for the win32 platform layer in C/C++",
+            image: ["https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/cplusplus/cplusplus-original.svg"],
+            link: "https://github.com/DillonRmD/SoftwareRenderer",
+        },
+        {
+            name: "Literally Just A Space Game With Cards",
+            description:
+                "A Collaborative Unity 2D, game project that features dynamic card playing in a vast universe just waiting to be conquered!",
+            image: ["https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/unity/unity-original.svg"],
+            link: "https://github.com/aking618/LJSGC",
+        },
+    ];
 
-  ];
-
-  return (
-    <section id="Project">
-      <div className="Project">
-        <div className="header">
-          <h1>My Projects</h1>
-        </div>
-
-        <div className= "Project-disclaimer">
-          <h1> Below are just some of my highlighted projects! </h1>
-          <h1> Feel free to to check out the rest of my projects by clicking below!</h1>
-          <h1><a href="https://github.com/DillonRmD/"> My super epic github profile</a></h1>
-        </div>
-
-        <div className="Project-entry">
-          {projects.map((project, projectIndex) => (
-            <div key = {projectIndex} >
-              <a href={project.link} key={project.image} target="_blank" rel="noreferrer">
-                <div className="Project-title">
-                  <h1>
-                    {project.name}
-                    <img
-                      alt={project.name}
-                      src={project.image}
-                      width="40"
-                      height="40"
-                    />
-                  </h1>
+    return (
+        <section id="Project">
+            <div className="Project">
+                <div className="header">
+                    <h1>My Projects</h1>
                 </div>
-              </a>
 
-              <div className="Project-desc">
-                <p>{project.description}</p>
-              </div>
+                <div className="Project-disclaimer">
+                    <h1> Below are just some of my highlighted projects! </h1>
+                    <h1>
+                        {" "}
+                        Feel free to to check out the rest of my projects by
+                        clicking below!
+                    </h1>
+                    <h1>
+                        <a href="https://github.com/DillonRmD/">
+                            {" "}
+                            My super epic github profile
+                        </a>
+                    </h1>
+                </div>
+
+                <div className="Project-entry">
+                    {projects.map((project, projectIndex) => (
+                        <div key={projectIndex}>
+                            <a
+                                href={project.link}
+                                key={project.image}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <div className="Project-title">
+                                    <h1>
+                                        {project.name}
+                                        {project.image.map((img, imgIndex) => (
+                                          <img
+                                            alt={project.name}
+                                            src={img}
+                                            width="40"
+                                          height="40"
+                                        />
+                                        ))}
+                                        
+                                    </h1>
+                                </div>
+                            </a>
+
+                            <div className="Project-desc">
+                                <p>{project.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 export default Project;
